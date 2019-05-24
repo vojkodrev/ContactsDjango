b013a3d407194c9311c996c85e303b6e601853e4
Vojko Drev <vojkodrev@gmail.com>
2019-05-24 17:53:51 +0200

    Output possible values.

--- contactsRest/models.py
+++ contactsRest/models.py
@@ -7,6 +7,10 @@ LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 
+print("LEXERS", LEXERS)
+print("LANGUAGE_CHOICES", LANGUAGE_CHOICES)
+print("STYLE_CHOICES", STYLE_CHOICES)
+
 
 class Snippet(models.Model):
   created = models.DateTimeField(auto_now_add=True)
