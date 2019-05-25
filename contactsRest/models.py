1461e8f61255cd44c42ba57207c8ca449479981f
Vojko Drev <vojkodrev@gmail.com>
2019-05-25 10:12:03 +0200

    Foreign keys.

--- contactsRest/models.py
+++ contactsRest/models.py
@@ -1,24 +1,18 @@
 from django.db import models
 
-from pygments.lexers import get_all_lexers
-from pygments.styles import get_all_styles
+class Album(models.Model):
+  album_name = models.CharField(max_length=100)
+  artist = models.CharField(max_length=100)
 
-LEXERS = [item for item in get_all_lexers() if item[1]]
-LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
-STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
-
-print("LEXERS", LEXERS)
-print("LANGUAGE_CHOICES", LANGUAGE_CHOICES)
-print("STYLE_CHOICES", STYLE_CHOICES)
-
-
-class Snippet(models.Model):
-  created = models.DateTimeField(auto_now_add=True)
-  title = models.CharField(max_length=100, blank=True, default='')
-  code = models.TextField()
-  linenos = models.BooleanField(default=False)
-  language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
-  style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+class Track(models.Model):
+  album = models.ForeignKey(Album, related_name='tracks', on_delete=models.CASCADE)
+  order = models.IntegerField()
+  title = models.CharField(max_length=100)
+  duration = models.IntegerField()
 
   class Meta:
-    ordering = ('created',)
+    unique_together = ('album', 'order')
+    ordering = ['order']
+
+  def __str__(self):
+    return '%d: %s' % (self.order, self.title)
--- contactsRest/urls.py
+++ contactsRest/urls.py
@@ -3,8 +3,7 @@ from contactsRest import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-  path('snippets/', views.SnippetList.as_view()),
-  path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+  path('albums/', views.AlbumList.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
